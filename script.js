1228defdc0527265bf529e4b1bfea48c9a510064
unknown <eskicmihajlo55@gmail.com>
2024-02-17 12:17:03 +0100

    created input logic

---
+++ script.js
@@ -0,0 +1,48 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+
+const decimalToBinary = (input) => {
+
+
+
+
+}
+
+
+const checkUserInput = () => {
+
+    
+
+    if (!numberInput.value || isNaN(parseInt(numberInput.value))) // if value empty TURE, if value not number ture
+    {
+       
+        alert("Please provide a decimal number");
+        return; /// break function
+
+    }
+
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
+
+
+
+
+
+}
+
+
+convertBtn.addEventListener("click" , checkUserInput);
+
+numberInput.addEventListener("keydown" , (e) => {
+
+   if (e.key == "Enter")
+   {
+
+    checkUserInput();
+
+   }
+
+
+})
